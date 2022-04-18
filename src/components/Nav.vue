9560c258e19388b63bf1ad4b74792cb0a24228d3
李鹏飞 <lee-perfect@qq.com>
2022-04-19 02:58:40 +0800

    Update:更改nav显示细节

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,31 +2,41 @@
   <div class="nav">
     <div class="container">
       <router-link :to="{ name: 'Home' }"
-        ><div class="logo-title">History</div></router-link
+        ><div class="logo-title">Prediction³</div></router-link
       >
       <div class="right">
-        <!-- <router-link :to="{ name: 'Bank' }">
-          <a-button size="small">Get Test Coin</a-button>
-        </router-link> -->
         <router-link :to="{ name: 'New' }"> Create Prediction </router-link>
-        <a-button class="connect-wallet" @click="connect_wallet" v-if="!wallet_address"
+        <a-button
+          class="connect-wallet"
+          @click="connect_wallet"
+          v-if="!wallet_address"
           >Connect Wallet</a-button
         >
         <div class="wallet btn" v-else>
-          <div class="balance">{{ balance }} {{networkInfo.token}}</div>
-          <div class="division"></div>
-          <div class="address">
-            {{
-              `${wallet_address.slice(0, 4)}...${wallet_address.slice(wallet_address.length - 4)}`
-            }}
+          <div class="wallet-line">
+            <div class="balance">{{ balance }} {{ networkInfo.token }}</div>
+            <div class="division"></div>
+            <div class="address">
+              {{
+                `${wallet_address.slice(0, 4)}...${wallet_address.slice(
+                  wallet_address.length - 4
+                )}`
+              }}
+            </div>
+            <avatar
+              variant="beam"
+              :size="20"
+              :name="wallet_address"
+              class="avatar"
+              :colors="color"
+            />
           </div>
-          <avatar
-            variant="beam"
-            :size="25"
-            :name="wallet_address"
-            class="avatar"
-            :colors="color"
-          />
+          <div class="chain-line">
+            <div class="line">{{ networkInfo.chainName }} <div class="dot"></div></div>
+            <div class="line" v-for="(net, key) in chain_list" :key="key">
+              {{ net.chainName }}
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -49,11 +59,8 @@ export default {
   },
   async mounted() {
     let self = this;
-   
   },
-  methods: {
-    
-  },
+  methods: {},
   computed: {
     user_address() {
       let self = this;
@@ -63,6 +70,15 @@ export default {
           0,
           5
         )}...${self.wallet_address.substring(l - 5, l)}`;
+      }
+    },
+    chain_list() {
+      let networks = Object.values(config.networks);
+      if (!this.networkInfo) {
+        return [];
+      } else {
+        let name = this.networkInfo.chainName;
+        return networks.filter((e) => e.chainName != name);
       }
     },
   },
@@ -95,25 +111,82 @@ export default {
     .wallet {
       margin-left: 20px;
       background-color: white;
-      border: 1px solid #d9d9d9;
-      display: flex;
       box-sizing: border-box;
-      height: 40px;
-      padding: 0 15px;
-      font-size: 14px;
       border-radius: 4px;
-      line-height: 40px;
-      align-items: center;
-      .division {
-        width: 1px;
-        height: 15px;
-        background-color: rgba(0, 0, 0, 0.45);
-        margin: 0px 10px;
+      position: relative;
+      width:203px;
+      &:hover .wallet-line {
+        border-bottom-color: transparent;
+        border-bottom-right-radius: unset;
+        border-bottom-left-radius: unset;
       }
-      .address {
+      &:hover .chain-line {
+        transform: scaleY(1);
+        transform-origin: top;
+        height:auto;
+        border: 1px solid #d9d9d9;
+        border-radius: 4px;
+
+        border-top-right-radius: unset;
+        border-top-left-radius: unset;
+        border-top: unset;
       }
-      .avatar {
-        margin-left: 5px;
+      .wallet-line {
+        height: 30px;
+        font-size: 12px;
+
+        border: 1px solid #d9d9d9;
+        padding: 0 15px;
+        line-height: 30px;
+        border-radius: 4px;
+
+        display: flex;
+        align-items: center;
+
+        .division {
+          width: 1px;
+          height: 15px;
+          background-color: rgba(0, 0, 0, 0.45);
+          margin: 0px 10px;
+        }
+
+        .avatar {
+          margin-left: 5px;
+        }
+      }
+      .chain-line {
+      width:203px;
+
+        position: absolute;
+        box-sizing: border-box;
+        background-color: white;
+        z-index: 1000;
+        
+        transform: scaleY(0);
+        transition: transform .2s ease-out;
+        height:0px;
+        overflow: hidden;
+        .line {
+          width:100%;
+          line-height: 30px;
+          padding: 0px 15px;
+          font-size: 12px;
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+          .dot{
+            height:8px;
+            width:8px;
+            border-radius: 50%;
+            background-color: #04C086;
+            margin-left: auto;
+            margin-right: 6px;
+          }
+          &:hover{
+            background-color: black;
+            color: white;
+          }
+        }
       }
     }
   }
